d04d2aea32d45f41d8d684e2eb6914e130ed84dd
Abhijeet Reddy <abhijeet_abhi@live.co.uk>
2020-03-20 15:06:02 +0530

    Removed redundant code.

--- background.js
+++ background.js
@@ -3,18 +3,6 @@ console.log('Code Sharing!!');
 var newURL = "https://codeshare.io/new";
 let codeshare_tab_id = 0;
 
-// function share_code(){
-//     var newURL = "https://codeshare.io/298ydM";
-//     chrome.tabs.create({ url: newURL }, (tab) => {
-//         tabid = tab.id;
-//         console.log(tab);
-//         // window.onload = function() {
-//         //     console.log('Pasting');
-//         //     document.execCommand('paste');
-//         // }
-//     });   
-// }
-
 chrome.browserAction.onClicked.addListener((tab) => {
     chrome.tabs.sendMessage(tab.id, {cmd: 'copy'});
     console.log('Code Copy request sent');
--- content.js
+++ content.js
@@ -1,42 +1,23 @@
 console.log('conent click');
 
-// document.addEventListener('select', function (evt) {
-//     console.log('selected', evt);
-// });
-
 function get_code() {
     let lines = document.querySelector('#codeshell-wrapper > div.hr_tour-code-solution.movable-hand.flex-row > div > div.code-editors > div.code-body > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div > div.CodeMirror-code');
     lines = lines.innerText;
 
     let code_arr = lines.split('\n');
-    // console.log(code_arr);
+
     let code = '';
     for(let i = 1; i < code_arr.length; i+=2){
-        // console.log(code_arr[i]);
 
         if(code_arr[i].charCodeAt(0) != 8203)
             code += code_arr[i];
         
         code += '\n';
-        // console.log(code);
     }
     
-    // alert(code);
     console.log(code);
     return code;
-    // let text_area = document.querySelector('#editor > div > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div > div.CodeMirror-code > div > pre');
-    // text_area.click();
-
-
 }
-
-// chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
-//     console.log('msg', msg);
-//     alert(msg.txt);
-
-//     console.log('Pasting');
-//     document.execCommand('paste', msg.txt);
-// })
 
 function copy_clipboard(text) {
     var textArea = document.createElement("textarea");
